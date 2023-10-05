8e4f77ec4e2f38aeff551a9796e83a3e764b625a
Soorya-dev <146190467+Soorya-dev@users.noreply.github.com>
2023-10-05 19:06:24 +0530

    Add files via upload

---
+++ disney/disney.js
@@ -0,0 +1,8 @@
+const toTop = document.getElementById("to-top");
+window.addEventListener("scroll", ()=> {
+    if(window.pageYOffset >100) {
+      toTop.classList.add("active");
+    }else{
+        toTop.classList.remove("active");
+    }
+})
